f03acfb39ca465e4119cba6048717e6c519570f4
brayanpasa99 <brayanpasa99@hotmail.com>
2022-02-13 20:11:10 -0500

    Upload database

---
+++ database/harrylibrary_book.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for Linux (x86_64)
+--
+-- Host: localhost    Database: harrylibrary
+-- ------------------------------------------------------
+-- Server version	8.0.28-0ubuntu0.20.04.3
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `book`
+--
+
+DROP TABLE IF EXISTS `book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `book` (
+  `id` bigint NOT NULL,
+  `image_url` varchar(255) DEFAULT NULL,
+  `price` double NOT NULL,
+  `stock` int DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book`
+--
+
+LOCK TABLES `book` WRITE;
+/*!40000 ALTER TABLE `book` DISABLE KEYS */;
+INSERT INTO `book` VALUES (1,'https://cdn-icons-png.flaticon.com/512/1601/1601065.png',41000,1,'Harry Potter y La Piedra Filosofal'),(3,'https://cdn-icons-png.flaticon.com/512/1601/1601065.png',39000,5,'Harry Potter y La Cámara Secreta'),(4,'https://cdn-icons-png.flaticon.com/512/1601/1601065.png',39000,8,'Harry Potter y El Prisionero de Azkaban'),(5,'https://cdn-icons-png.flaticon.com/512/1601/1601065.png',60000,9,'Harry Potter y El Cáliz de Fuego'),(6,'https://cdn-icons-png.flaticon.com/512/1601/1601065.png',75000,9,'Harry Potter y La Orden del Fénix'),(7,'https://cdn-icons-png.flaticon.com/512/1601/1601065.png',40000,9,'Harry Potter y El Misterio del Príncipe'),(8,'https://cdn-icons-png.flaticon.com/512/1601/1601065.png',42000,9,'Harry Potter y Las Reliquias de la Muerte'),(9,'https://cdn-icons-png.flaticon.com/512/1601/1601065.png',55000,9,'Harry Potter y El Legado Maldito');
+/*!40000 ALTER TABLE `book` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-02-13 20:04:03
